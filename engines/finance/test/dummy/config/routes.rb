1e8927c9977c979997ba9f2b6fadf9afcdac5a93
Felipe Felix <felipe.felix@maino.com.br>
2024-09-15 21:14:40 -0300

    chore: remover pasta tests

--- engines/finance/test/dummy/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount Finance::Engine => "/finance"
-end
--- engines/marketing/test/dummy/app/assets/config/manifest.js
+++
@@ -1,3 +0,0 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link marketing_manifest.js
