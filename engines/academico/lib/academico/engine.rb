8d8b893175ef955b692f6ded555f168741fc7619
Felipe Felix <felipe.felix@maino.com.br>
2024-09-29 14:49:05 -0300

    chore: remover autoload das views desnecessário

--- engines/academico/lib/academico/engine.rb
+++ engines/academico/lib/academico/engine.rb
@@ -2,7 +2,6 @@ module Academico
   class Engine < ::Rails::Engine
     config.autoload_paths << "#{Academico::Engine.root}/app/infra/http/controllers"
     config.autoload_paths << "#{Academico::Engine.root}/app/infra/http/helpers"
-    config.autoload_paths << "#{Academico::Engine.root}/app/infra/http/views"
     config.autoload_paths << "#{Academico::Engine.root}/app/infra/repositories"
     config.autoload_paths << "#{Academico::Engine.root}/app/application"
     config.autoload_paths << "#{Academico::Engine.root}/app/domain"
