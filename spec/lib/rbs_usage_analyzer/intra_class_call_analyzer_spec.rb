2cf02fdbec7d718b27df4d13e0527a22ce8ef3d7
Felipe Felix <felippe.felix98@gmail.com>
2026-03-15 04:33:09 -0300

    feat: infere tipos de params via usage-side (Klass.new(param:) no corpo do método)
    Quando um método recebe kwargs e os repassa para Klass.new(param:),
    o IntraClassCallAnalyzer agora infere os tipos dos parâmetros a partir
    da assinatura da classe alvo via resolve_all.
    
    Exemplo: adicionar_telefone(ddd:, numero:) com corpo Telefone.new(ddd:, numero:)
    → ddd: String, numero: String (inferido dos attrs de Telefone)
    
    Adiciona 4 testes para o novo padrão usage-side.

--- spec/lib/rbs_usage_analyzer/intra_class_call_analyzer_spec.rb
+++ spec/lib/rbs_usage_analyzer/intra_class_call_analyzer_spec.rb
@@ -68,4 +68,76 @@ RSpec.describe RbsUsageAnalyzer::IntraClassCallAnalyzer do
     expect(visitor.inferred_param_types["matricular"]["aluno"]).to eq("Entity")
     expect(visitor.inferred_param_types["matricular"]["curso"]).to eq("Curso")
   end
+
+  context "usage-side: infere tipos de params via Klass.new(param:) no corpo" do
+    let(:resolver) do
+      instance_double(RbsUsageAnalyzer::MethodTypeResolver).tap do |r|
+        allow(r).to receive(:resolve_all).with("Telefone").and_return({
+          "ddd" => "String",
+          "numero" => "String"
+        })
+      end
+    end
+
+    it "infere tipo de param quando forwarded via shorthand para Klass.new(param:)" do
+      source = <<~RUBY
+        class Foo
+          def adicionar_telefone(ddd:, numero:)
+            Telefone.new(ddd:, numero:)
+          end
+        end
+      RUBY
+
+      visitor = analyze(source, method_type_resolver: resolver)
+      expect(visitor.inferred_param_types["adicionar_telefone"]["ddd"]).to eq("String")
+      expect(visitor.inferred_param_types["adicionar_telefone"]["numero"]).to eq("String")
+    end
+
+    it "infere tipo via param: param explícito em Klass.new" do
+      source = <<~RUBY
+        class Foo
+          def adicionar(codigo:)
+            Telefone.new(ddd: codigo)
+          end
+        end
+      RUBY
+
+      resolver_local = instance_double(RbsUsageAnalyzer::MethodTypeResolver)
+      allow(resolver_local).to receive(:resolve_all).with("Telefone").and_return({
+        "ddd" => "String",
+        "numero" => "String"
+      })
+
+      visitor = analyze(source, method_type_resolver: resolver_local)
+      expect(visitor.inferred_param_types["adicionar"]["codigo"]).to eq("String")
+    end
+
+    it "não infere quando o valor não é um parâmetro do método" do
+      source = <<~RUBY
+        class Foo
+          def adicionar(ddd:)
+            local = "11"
+            Telefone.new(ddd:, numero: local)
+          end
+        end
+      RUBY
+
+      visitor = analyze(source, method_type_resolver: resolver)
+      expect(visitor.inferred_param_types["adicionar"]["ddd"]).to eq("String")
+      expect(visitor.inferred_param_types["adicionar"]).not_to have_key("numero")
+    end
+
+    it "não infere sem method_type_resolver" do
+      source = <<~RUBY
+        class Foo
+          def adicionar(ddd:)
+            Telefone.new(ddd:)
+          end
+        end
+      RUBY
+
+      visitor = analyze(source)
+      expect(visitor.inferred_param_types["adicionar"]).to be_empty
+    end
+  end
 end
